7edbfdd5d510247b6dd36452f25faee35120019f
Phan Anh Tú <124888378+tuphan22028238@users.noreply.github.com>
2024-10-24 14:43:45 +0700

    modified preprocessing modules and upload output files of processing (csv file)

--- preprocess/simple_processing.py
+++ preprocess/simple_processing.py
@@ -1,69 +1,79 @@
-from typing import List, Dict
-
 import pandas as pd
 
-from clean_spoken import clean_spoken
-from group_dialogue import group_dialogue
-from check_role import fix_role
-from restore_punctuation import restore_punc
-from model_manager import get_spacy_model
-from util import Utterance, init_from_str_dialogue
+from ConversationRoleNormalization import fix_role
+from DialogueChunking import chunk_dialogues_from_df
+from PunctuationRestoration import restore_punc
+from UtteranceNormalization import clean_spoken
 
+CLEAN_CONTRACTIONS = {
+    "can't": "cannot",
+    "won't": "will not",
+    "I'm": "I am",
+    "isn't": "is not",
+    "aren't": "are not",
+    "it's": "it is",
+    "I'll": "I will",
+    "we'll": "we will",
+    "don't": "do not",
+    "didn't": "did not",
+    "ca n't": "can not",
+    "wo not": "will not",
+    "n't": "not",
+    "could've": "could have",
+    "i'm": "i am",
+    "i've": "i have",
+    "might've": "might have",
+    "must've": "must have",
+    "shan't": "shall not",
+    "should've": "should have",
+    "they're": "they are",
+    "they've": "they have",
+    "wasn't": "was not",
+    "we're": "we are",
+    "we've": "we have",
+    "what're": "what are",
+    "what've": "what have",
+    "who're": "who are",
+    "who've": "who have",
+    "would've": "would have",
+    "you're": "you are",
+    "you've": "you have",
+    "gonna": "going to",
+    "gon'na": "going to",
+    "gon na": "going to",
+    "gotta": "got to",
+    "wanna": "want to",
+    "wan'na": "want to",
+    "wan na": "want to",
+    "hafta": "have to",
+    "hadta": "had to",
+    "shoulda": "should have",
+    "woulda": "would have",
+    "coulda": "could have",
+    "mighta": "might have",
+    "musta": "must have",
+    "oughta": "ought to",
+    "dont": "do not",
+    "doesnt": "does not",
+    "uh": "",
+    "um": "",
+    "er": "",
+    "ah": "",
+    "well": "",
+    "you know": "",
+    "I mean": "",
+    "like": "",
+    "hmm": ""
 
-def statistic_each_dialogue(dialogue: str, task: str):
-    """
-    Describe a dialogue based on:
-        - Total length in word; sentence; uterance
-        - uterances/role
-        - words/uterance/role; sentences/uterance/role
-        - NER/uterance/role
-    """
-    figures = {}
-    uterances = init_from_str_dialogue(dialogue, task=task)
-    figures['Dialogue length in uterance'] = len(uterances)
-    figures['Dialogue length in word'] = sum([len(each.spacy_doc) for each in uterances])
-    figures['Dialogue length in sentence'] = sum([len(list(each.spacy_doc.sents)) for each in uterances])
-    to_merge_figures = {}
-    roles = {}
-    for each in uterances:
-        if each.role not in roles:
-            roles[each.role] = 1
-        else:
-            roles[each.role] += 1
-        if f'Uterance length in word of {each.role}' not in to_merge_figures:
-            to_merge_figures[f'Uterance length in word of {each.role}'] = []
-        to_merge_figures[f'Uterance length in word of {each.role}'].append(len(each.spacy_doc))
-        if f'Uterance length in sentence of {each.role}' not in to_merge_figures:
-            to_merge_figures[f'Uterance length in sentence of {each.role}'] = []
-        to_merge_figures[f'Uterance length in sentence of {each.role}'].append(len(list(each.spacy_doc.sents)))
-        if f'Number of named entities in uterance of {each.role}' not in to_merge_figures:
-            to_merge_figures[f'Number of named entities in uterance of {each.role}'] = []
-        to_merge_figures[f'Number of named entities in uterance of {each.role}'].append(len(list(each.spacy_doc.ents)))
+}
 
-    for role, num in roles.items():
-        figures[f'Dialogue length in uterance of {role}'] = num
-    return figures, to_merge_figures, uterances
+input_file_path = "TaskC-TrainingSet.csv"
+output_file_path = "[Final]valid_chunked_output_file.csv"
+dialogue_column = "dialogue"
 
-
-def describe_text(text):
-    """
-    Describe a text
-    """
-    nlp = get_spacy_model()
-    spacy_doc = nlp(text)
-    figures = {}
-    figures['Number of words'] = len(spacy_doc)
-    figures['Number of sentences'] = len(list(spacy_doc.sents))
-    figures['Number of entities'] = len(list(spacy_doc.ents))
-    to_merge = {}
-    to_merge['Sentence length in word'] = [len(sent) for sent in spacy_doc.sents]
-    return figures, to_merge
-
-
-def preprocessing(dialogue_list: List[List[Utterance]],
-                  df: pd.DataFrame,
+def preprocessing(df: pd.DataFrame,
                   dialogue_column: str,
-                  **kwargs) -> Dict:
+                  **kwargs) -> pd.DataFrame:
     """
     Preprocessing function.
     Receive dialogue data (raw) of a list of dialogue, this function return a tuple of pre-processed dialogue, other meta data, etc.
@@ -71,9 +81,18 @@ def preprocessing(dialogue_list: List[List[Utterance]],
     assert dialogue_column in df.columns, f"Expected column '{dialogue_column}' in the dataframe"
     df_cleaned = clean_spoken(df, dialogue_column=dialogue_column, **kwargs)
     df_punc = restore_punc(df_cleaned, dialogue_column='clean_dialogue', **kwargs)
-    df_fix_role = fix_role(df_punc, dialogue_column='restore_punctuation_dialogue', **kwargs)
-    df_group = group_dialogue(df_fix_role, dialogue_column='fixed_role_dialogue', **kwargs)
+    df_role = fix_role(df_punc, dialogue_column='restore_punctuation_dialogue', **kwargs)
+    df_chunked = chunk_dialogues_from_df(df_role, dialogue_column='fixed_role_dialogue', **kwargs)
 
-    return {
-        "dialogue_list": dialogue_list, "df": df_group
-    }
+    return df_chunked
+
+if __name__ == "__main__":
+    # Read dataset
+    df = pd.read_csv(input_file_path)
+
+    # Preprocessing dataset
+    df_processed = preprocessing(df, dialogue_column, CLEAN_CONTRACTIONS=CLEAN_CONTRACTIONS)
+
+    # Save DataFrame after processing to .csv file (optional)
+    df_processed.to_csv(output_file_path, index=False)
+    print(f"Processed dataset saved to {output_file_path}")
